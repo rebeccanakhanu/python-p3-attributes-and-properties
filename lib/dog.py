24d10812859b28a199f60f5154c8b27880336fde
rebeccanakhanu <rebeccanakhanu@gmail.com>
2023-06-02 21:06:19 +0300

    completed assignments

--- lib/dog.py
+++ lib/dog.py
@@ -1,15 +1,37 @@
-#!/usr/bin/env python3
-
-APPROVED_BREEDS = [
-    "Mastiff",
-    "Chihuahua",
-    "Corgi",
-    "Shar Pei",
-    "Beagle",
-    "French Bulldog",
-    "Pug",
-    "Pointer"
-]
-
 class Dog:
-    pass
+    APPROVED_BREEDS = [
+        "Mastiff",
+        "Chihuahua",
+        "Corgi",
+        "Shar Pei",
+        "Beagle",
+        "French Bulldog",
+        "Pug",
+        "Pointer",
+    ]
+
+    def __init__(self):
+        self._name = None
+        self._breed = None
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 0 < len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value in self.APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in the list of approved breeds.")
--- lib/person.py
+++ lib/person.py
@@ -15,5 +15,29 @@ APPROVED_JOBS = [
     "Purchasing"
 ]
 
+
 class Person:
     pass
+    def __init__(self):
+        self._name = None
+        self._job = "Programmer"
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 0 < len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+    @property
+    def job(self):
+        return self._job
+    @job.setter
+    def job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in the list of approved jobs.")
+
+   
